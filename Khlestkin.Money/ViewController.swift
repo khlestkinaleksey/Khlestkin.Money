776ae9e7352d2780db50ba768adb1857d2fbe14f
varshaver <varshaveraleksey@gmail.com>
2023-06-06 02:24:34 +0300

    Initial Commit

---
+++ Khlestkin.Money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Khlestkin.Money
+//
+//  Created by 1234 on 06.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
