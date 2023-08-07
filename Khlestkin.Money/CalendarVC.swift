2b722f02bac88b9fd2113230d2a67835409d5a21
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-08-08 01:21:59 +0300

    111

--- Khlestkin.Money/CalendarVC.swift
+++ Khlestkin.Money/CalendarVC.swift
@@ -12,6 +12,7 @@ class CalendarVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("hello")
         // Do any additional setup after loading the view.
     }
     
